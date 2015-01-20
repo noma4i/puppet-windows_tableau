93dfdbdbcd11a8717543761c86737f95d5149731
Alexander Tsirel <noma4i@gmail.com>
2015-01-20 14:33:40 +1100

    Cleanup to pass puppet linting

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,44 +10,44 @@ class windows_tableau (
       if $osfamily == 'windows' {
         File { source_permissions => ignore }
         if $type == 'local'{
-          $tablue_path = "C:\\Users\\Administrator\\Desktop\\TableauServer-64bit.exe"
+          $tablue_path = 'C:\\Users\\Administrator\\Desktop\\TableauServer-64bit.exe'
           file { 'c:\\ProgramData\\tableau.exe':
-            ensure  => file,
-            source_permissions => ignore,
-            source  => "puppet:///${from}"
+            ensure              => file,
+            source_permissions  => ignore,
+            source              => "puppet:///${from}"
           }
         }
         if $type == 'remote'{
           download_file { "Download Tableau" :
-            url => $from,
+            url                   => $from,
             destination_directory => 'c:\\ProgramData',
-            destination_file => 'tableau.exe',
-            timeout => 90000
+            destination_file      => 'tableau.exe',
+            timeout               => 90000
           }
-          $tablue_path = "C:\Users\Administrator\Desktop\TableauServer-64bit1.exe"
+          $tablue_path = 'C:\Users\Administrator\Desktop\TableauServer-64bit1.exe'
         }
         exec { 'Setup Tableau':
           command => "${tablue_path} /verysilent",
-          alias => 'tableau-setup',
+          alias   => 'tableau-setup',
           creates => 'C:\Program Files\Tableau\Tableau Server\unins000.dat',
-          provider => powershell,
+          provide => powershell,
           # extended timeout cause Tableau is SLOW!
           timeout => 90000
         }->
         exec { 'perform Tableau Setup':
-          command  => template('windows_tableau/initial_setup.ps1.erb'),
-          creates => 'C:\Program Files\Tableau\Tableau Server\initial_setup.flg',
-          provider => 'powershell',
-          timeout => 600
+          command   => template('windows_tableau/initial_setup.ps1.erb'),
+          creates   => 'C:\Program Files\Tableau\Tableau Server\initial_setup.flg',
+          provider  => 'powershell',
+          timeout   => 600
         }->
         file { 'C:\\Program Files\\Tableau\\Tableau Server\\initial_setup.flg':
-          ensure  => file,
-          source_permissions => ignore
+          ensure              => file,
+          source_permissions  => ignore
         }
       }
     }
     default: {
-      fail("Invalid ensure option!\n")
+      fail('Invalid ensure option!\n')
     }
   }
 }
